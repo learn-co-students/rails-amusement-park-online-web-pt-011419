a13c141947de31222915ac38174e8da7b8a62f26
dclements9 <dclements@protonmail.com>
2019-07-10 16:05:12 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,29 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :attraction
+
+    def take_ride
+        if valid_ticket_amount && valid_height
+            return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+        elsif valid_ticket_amount
+            return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+        elsif valid_height
+            return "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+        else
+            self.user.tickets -= self.attraction.tickets
+            user.happiness += attraction.happiness_rating
+            user.nausea += attraction.nausea_rating
+
+            user.save
+            "Thanks for riding the #{self.attraction.name}!"
+        end
+    end
+
+    def valid_height
+        self.user.height < self.attraction.min_height
+    end
+
+    def valid_ticket_amount
+        self.user.tickets < self.attraction.tickets
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'users#new'
+  
+  resources :attractions do
+    resources :users
+  end
+
+  resources :users
+  resources :sessions, except: [:new, :create]
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 end
